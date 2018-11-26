301d0d0dea97bef7b23005df0234b8c72d2bb8f0
kei417 <kei.nak417@gmail.com>
2018-11-27 06:56:51 +0900

    [mod] cid 追加 [mod] コメント追加

--- examples/01.AzureIoTHub/STM32MQTT/STM32MQTT.ino
+++ examples/01.AzureIoTHub/STM32MQTT/STM32MQTT.ino
@@ -1,69 +1,61 @@
-/*
- B-L475E-IOT01A MQTT example to publish on https://io.adafruit.com broker
- See https://learn.adafruit.com/mqtt-adafruit-io-and-you/overview
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the B-L475E-IOT01A board.
- It will use several components of the board and you need to install corresponding libraries :
-   - WiFi (ISM43362-M3G-L44) : https://github.com/stm32duino/WiFi-ISM43362-M3G-L44
-   - Temperature and humidity sensor (HTS221) : https://github.com/stm32duino/HTS221
-   - Arduino Client for MQTT: https://github.com/knolleary/pubsubclient
- You can find more information on this board here :
-   http://www.st.com/en/evaluation-tools/b-l475e-iot01a.html
- You will need to create some feeds in your Adafruit IO Dashboard.
- See https://learn.adafruit.com/mqtt-adafruit-io-and-you/getting-started-on-adafruit-io
- to know how register and create your Adafruit IO dashboard and feeds:
-   - hello: Text block
-   - onoff: Toggle block
-   - temp: Stream block
-   - hum: Gauge block
- A screenshot of the dashboard is available in the sketch directory: dashboard_adafruit.png
-  It connects to the Adafruit IO's MQTT server (a.k.a broker) server then:
-  - publishes announcement "Hi, I'm STM32 user!" to the topic 'AIO_USERNAME"/feeds/hello"'
-  - subscribes to the topic AIO_USERNAME"/feeds/onoff"', switching LED_BUILTIN state
-  - publishes temperature and humidity from the HTS221 sensor to the topic
-    'AIO_USERNAME"/feeds/temp"' and 'AIO_USERNAME"/feeds/hum"' every 10 seconds
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
+/**
+ * @file  Arduino_MQTT_Temperature.ino
+ * 
+ * @brief MQTT経由で温度・湿度センサーの値をモニタリングします
+ *
+ * @author IoT Algyan in Hiroshima. K.Nakamura.
+ *
+ * @date   2018.11.03
 */
+
+/** 
+ * 温度・湿度センサー : HTS221
+ * ライブラリ         : STM32duino_HTS221
+ * 
+ * WIFIドライバー     : STM32duino_ISM43362-M3G-L44
+ * MQTTクライアント   : PubSubClient
+ */
+
 #include <SPI.h>
 #include <WiFiST.h>
 #include <PubSubClient.h>
 #include <HTS221Sensor.h>
-// LPS22HBセンサーライブラリをインクルード
 #include <LPS22HBSensor.h>
-// ３軸地磁気センサーライブラリ
 #include <LIS3MDLSensor.h>
-// Ｍａｔｈヘッダ
 #include <math.h>
 
 
-// Update these with values suitable for your network.
-//char* ssid              = "SPWN_H36_66D7AE";
-//const char* password    = "tgfd3ad4jagb931";
-//char* ssid              = "kei.nak5";
-//const char* password    = "kei.nak5";
+// WiFi 設定 (SSID)
 char* ssid              = "Buffalo-G-AE22";
+//char* ssid              = "kei.nak5";
+// WiFi 設定 (パスワード)
 const char* password    = "12345678";
-
-// Adafruit.io Setup
+//const char* password    = "kei.nak5";
+// MQTT サーバ (ＩＰアドレス)
 #define AIO_SERVER      "192.168.11.2"
-//#define AIO_SERVER      "192.168.137.1"
-//#define AIO_SERVER      "192.168.100.103"
+// MQTT サーバ (ポート番号)
 #define AIO_SERVERPORT  1883
+// MQTT サーバ (ユーザーＩＤ)
 #define AIO_USERNAME    "user"
+// MQTT サーバ (パスワード)
 #define AIO_KEY         "pass"
 
-// WiFi module setup
+// SPIバス接続ライブラリ(WiFiで使用)
 SPIClass SPI_3(PC12, PC11, PC10);
+
+// FiWiモジュールの定義
 WiFiClass WiFi(&SPI_3, PE0, PE1, PE8, PB13);
 WiFiClient STClient;
-int status = WL_IDLE_STATUS; // the Wifi radio's status
+
+// FiWiステータス
+int status = WL_IDLE_STATUS;
+
+// HTS221センサーオブジェクト
+HTS221Sensor  *HumTemp;
 
 // LPS22HBセンサーオブジェクト
 LPS22HBSensor *PressTemp;
 
-HTS221Sensor  *HumTemp;
 // ３軸地磁気センサー
 LIS3MDLSensor *Magneto;
 
@@ -72,25 +64,34 @@ TwoWire *dev_i2c;
 #define I2C2_SDA    PB11
 
 
-// i2c sensors
+// MQTTクライアント
 PubSubClient client(STClient);
 long lastMsg = 0;
-char msg[8];
-char msg2[8];
 char publishMsg[512];
+char cid[32] = {0};
 
+/**
+ * セットアップ
+ */
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT); // Initialize the LED_BUILTIN pin as an output
+  // LEDピン設定
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // シリアル通信初期化
   Serial.begin(115200);
+
+  //  WiFi のセットアップ
   setup_wifi();
+
+  // MQTTクライアント接続設定
   client.setServer(AIO_SERVER, AIO_SERVERPORT);
   client.setCallback(callback);
 
-  // Initialize I2C bus.
+  // I2Cバス接続ライブラリの初期化
   dev_i2c = new TwoWire(I2C2_SDA, I2C2_SCL);
   dev_i2c->begin();
 
-  // Initlialize components.
+  // 温度・湿度センサーライブラリの初期化
   HumTemp = new HTS221Sensor (dev_i2c);
   HumTemp->Enable();
 
@@ -104,41 +105,48 @@ void setup() {
   Magneto->Enable();
 }
 
+/**
+ *  WiFi のセットアップ
+ */
 void setup_wifi() {
 
   delay(10);
 
-  // Initialize the WiFi module:
+  // WiFiステータスのチェック (WL_NO_SHIELD の場合は起動できない)
   if (WiFi.status() == WL_NO_SHIELD) {
     Serial.println("WiFi module not detected");
     // don't continue:
     while (true);
   }
 
-  // Print firmware version:
+  // WiFiファームウェアバージョンの取得とチェック
   String fv = WiFi.firmwareVersion();
   Serial.print("Firmware version: ");
   Serial.println(fv);
 
-//  if (fv != "C3.5.2.3.BETA9") {
-//    Serial.println("Please upgrade the firmware");
-//  }
 
-  // Attempt to connect to Wifi network:
+  // WiFiが接続できるまでループする
   Serial.print("Attempting to connect to network: ");
   Serial.println(ssid);
   while (status != WL_CONNECTED) {
     Serial.print(".");
-    // Connect to network:
+    // WiFiネットワークに接続
     status = WiFi.begin(ssid, password);
-    // Wait 10 seconds for connection:
-    delay(10000);
+    // ２秒ウェイト
+    delay(2000);
   }
 
   Serial.println();
   Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.print("SSID : ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+  // クライアントIDを作成
+  sprintf(cid, "cid_%d", (int)WiFi.localIP()[3]);
+  Serial.print("cid : ");
+  Serial.println(cid);
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -164,22 +172,23 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
 }
 
+/**
+ * MQTT接続(再接続)
+ * MQTTは切断される場合があるので切断されている場合に再接続を行う処理が必須となる。
+ */
 void reconnect() {
-  // Loop until we're reconnected
+  // MQTT接続ループ
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    // Note - the default maximum packet size is 128 bytes. If the
-    // combined length of clientId, username and password exceed this,
-    // you will need to increase the value of MQTT_MAX_PACKET_SIZE in
-    // PubSubClient.h
+    // MQTT接続処理
     if (client.connect("STM32Client", AIO_USERNAME, AIO_KEY)) {
       Serial.println("connected");
-      // Once connected, publish an announcement...
+      // 接続時のメッセージ (以下は Topicが "user/feeds/hello", Payloadが "Hi, I'm STM32 user!"となる)
       client.publish(AIO_USERNAME"/feeds/hello", "Hi, I'm STM32 user!");
-      // ... and resubscribe
+      // resubscribe 購読設定
       client.subscribe(AIO_USERNAME"/feeds/onoff");
     } else {
+      // 失敗した場合は  5 秒 スリープして再チャレンジ
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -189,38 +198,38 @@ void reconnect() {
   }
 }
 
+/**
+ * メインループ処理
+ */
 void loop() {
-  float temperature, humidity;
+  float temperature = 0.0;
+  float humidity = 0.0;
+  float pressure = 0.0;
+  // ３軸地磁気センサデータ用バッファ [0] -> X, [1] -> Y, [2] -> Z に対応
+  int32_t magnetometer[3] = {0};
+
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
 
+  // 前回から 10000msec (10秒) 経過していれば、Publishメッセージを送信
   long now = millis();
   if (now - lastMsg > 10000) {
     lastMsg = now;
+
+    // 気温の取得
     HumTemp->GetTemperature(&temperature);
-//    dtostrf(temperature, 2, 2, msg);
-//    Serial.print("Publish temperature ");
-//    Serial.println(msg);
-
+    // 湿度の取得
     HumTemp->GetHumidity(&humidity);
-//    snprintf (msg, 8, "%u", (unsigned int)humidity);
-//    Serial.print("Publish humidity ");
-//    Serial.println(msg);
     // 気圧の取得
-    float pressure = 0.0;
     PressTemp->GetPressure(&pressure);
-
-    // ３軸地磁気センサデータ用バッファ [0] -> X, [1] -> Y, [2] -> Z に対応
-    int32_t magnetometer[3] = {0};
     // ３軸地磁気センサのセンサデータの読み出し
     Magneto->GetAxes(magnetometer);
 
-    dtostrf(temperature, 2, 2, msg);
-    dtostrf(pressure, 2, 2, msg2);
-    
-    snprintf(publishMsg, 512, "{\"temperature\":%s, \"humidity\":%u, \"pressure\":%s, \"MagX\":%d, \"MagY\":%d, \"MagZ\":%d}", msg, (unsigned int)humidity, msg2, magnetometer[0], magnetometer[1], magnetometer[2]);
+    // publishメッセージ(json)の構築
+    snprintf(publishMsg, 512, "{\"cid\":%s,\"temperature\":%4.1f, \"humidity\":%u, \"pressure\":%4.1f, \"MagX\":%d, \"MagY\":%d, \"MagZ\":%d}",
+            cid, temperature, (unsigned int)humidity, pressure, magnetometer[0], magnetometer[1], magnetometer[2]);
 
     // publish メッセージを送信
     client.publish(AIO_USERNAME"/feeds/data", publishMsg);
